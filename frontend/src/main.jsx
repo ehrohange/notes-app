9f4d2c28ceb2480e03dc78cc249045c07f6e3422
ehrohange <jorgeangelopangilinan@gmail.com>
2025-07-18 23:18:26 +0800

    authentication not finished yet

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,23 +1,28 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "react-auth-kit";
+import AuthProvider from 'react-auth-kit';
 import { Toaster } from "react-hot-toast";
 import App from "./App.jsx";
 import "./index.css";
+import createStore from 'react-auth-kit/createStore';
+
+const store = createStore({
+  authName:'_auth',
+  authType:'cookie',
+  cookieDomain: window.location.hostname,
+  cookieSecure: window.location.protocol === 'https:',
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <AuthProvider
-      authType="localstorage"
-      authName="_auth"
-      cookieDomain={window.location.hostname}
-      cookieSecure={false}
-    > */}
-      <BrowserRouter>
+    <BrowserRouter>
+      <AuthProvider
+        store={store}
+      >
         <App />
         <Toaster />
-      </BrowserRouter>
-    {/* </AuthProvider> */}
+      </AuthProvider>
+    </BrowserRouter>
   </StrictMode>
 );
